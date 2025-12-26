8e0db05aeff133cac7363a09b5f2aec517e5e7f6
krish-1010 <mkrishna.inbox@gmail.com>
2025-12-26 09:35:32 +0530

    update API base URL and add Vercel rewrites configuration

--- src/lib/api.js
+++ src/lib/api.js
@@ -2,7 +2,8 @@ import axios from "axios";
 
 const api = axios.create({
   // baseURL: "http://localhost:5000/api", // Your Node Backend URL
-  baseURL: "https://finance-backend-ww5g.onrender.com/api", // Your Node Backend URL
+  // baseURL: "https://finance-backend-ww5g.onrender.com/api", // Your Node Backend URL
+  baseURL: "/api", // Your Node Backend URL
   withCredentials: true, // IMPORTANT: Allows cookies (Session ID) to travel
 });
 
